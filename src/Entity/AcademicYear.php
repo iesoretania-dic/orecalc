7369bcd8bd0fedcc5710cad0f2e7b1d8c9189665
Luis Ramón López <lrlopez@gmail.com>
2025-10-16 19:58:28 +0200

    Añadida fecha de inicio y fin del curso académico para validación

--- src/Entity/AcademicYear.php
+++ src/Entity/AcademicYear.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\AcademicYearRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AcademicYearRepository::class)]
@@ -15,6 +16,12 @@ class AcademicYear
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $start = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $end = null;
 
     public function getId(): ?int
     {
@@ -29,6 +36,30 @@ class AcademicYear
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTime
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTime $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTime
+    {
+        return $this->end;
+    }
+
+    public function setEnd(\DateTime $end): static
+    {
+        $this->end = $end;
 
         return $this;
     }
